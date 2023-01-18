00e307d728f6b6939785352105fe8af1d7b13ce6
Илья Остапенко <6londo9@MacBook-Pro-Ila.local>
2023-01-18 21:38:20 +1100

    add template engine

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,24 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
-import io.javalin.validation.ValidationException;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "5000");
         return Integer.valueOf(port);
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
     }
 
     private static void addRoutes(Javalin app) {
@@ -15,12 +27,27 @@ public class App {
 
     }
 
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
     public static Javalin getApp() {
 
-        Javalin app = Javalin.create(config -> config.plugins.enableDevLogging());
-
-        app.exception(ValidationException.class, (e, ctx) -> {
-            ctx.json(e.getErrors()).status(422);
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
         });
 
         addRoutes(app);
