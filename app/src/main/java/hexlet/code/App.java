f3f0e7dab2fda8b3981c67369a56a463f1bf0c94
Илья Остапенко <6londo9@MacBook-Pro-Ila.local>
2023-01-19 22:33:36 +1100

    fix project structure

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,6 +11,7 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import hexlet.code.controllers.RootController;
 
 public class App {
+
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "5000");
         return Integer.valueOf(port);
@@ -33,17 +34,18 @@ public class App {
     private static TemplateEngine getTemplateEngine() {
         TemplateEngine templateEngine = new TemplateEngine();
 
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setPrefix("/templates/");
 
         templateEngine.addTemplateResolver(templateResolver);
-        templateEngine.addDialect(new LayoutDialect());
-        templateEngine.addDialect(new Java8TimeDialect());
 
         return templateEngine;
     }
 
-    public static Javalin getApp() {
+    private static Javalin getApp() {
 
         Javalin app = Javalin.create(config -> {
             if (!isProduction()) {
