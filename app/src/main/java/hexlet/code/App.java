efe8cf240ca0e1c0254c747c7c50e69f2a049940
Илья Остапенко <6londo9@MacBook-Pro-Ila.local>
2023-01-22 22:50:05 +1100

    add start, urls and url pages

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.controllers.UrlController;
 import io.javalin.Javalin;
 import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 
@@ -9,6 +10,8 @@ import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 import hexlet.code.controllers.RootController;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class App {
 
@@ -28,6 +31,14 @@ public class App {
     private static void addRoutes(Javalin app) {
 
         app.get("/", RootController.welcome);
+
+        app.routes(() -> {
+            path("urls", () -> {
+                get(UrlController.listUrls);
+                post(UrlController.createUrl);
+                get("{id}", UrlController.showUrl);
+            });
+        });
 
     }
 
---
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -0,0 +1,81 @@
+package hexlet.code.controllers;
+
+import io.javalin.core.validation.JavalinValidation;
+import io.javalin.core.validation.ValidationError;
+import io.javalin.core.validation.Validator;
+import io.javalin.http.Handler;
+
+import java.util.List;
+import java.util.Map;
+import java.net.URL;
+
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
+
+public class UrlController {
+
+    public static Handler listUrls = ctx -> {
+
+        List<Url> urls = new QUrl()
+                .orderBy()
+                    .id.asc()
+                .findList();
+
+        ctx.attribute("urls", urls);
+        ctx.render("urls/index.html");
+    };
+
+    public static Handler showUrl = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        ctx.attribute("url", url);
+        ctx.render("urls/show.html");
+    };
+
+    public static Handler createUrl = ctx -> {
+
+        String url = "";
+
+        try {
+            URL incomeUrl = new URL(ctx.formParam("url"));
+            url = new StringBuilder()
+                    .append(incomeUrl.getProtocol())
+                    .append("://")
+                    .append(incomeUrl.getAuthority())
+                    .toString();
+
+        } catch (Exception e) {
+
+        }
+
+        Validator<String> urlValidator = new Validator<>(url, String.class, "url")
+                .check(value -> !value.equals(""), "Некорректный URL");
+
+        Map<String, List<ValidationError<?>>> errors = JavalinValidation.collectErrors(urlValidator);
+
+        if (!errors.isEmpty()) {
+            ctx.status(422);
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.redirect("/");
+            return;
+        }
+
+        Url isExist = new QUrl()
+                .name.equalTo(url)
+                .findOne();
+
+        if (isExist != null) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+
+        } else {
+            Url newUrl = new Url(url);
+            newUrl.save();
+            ctx.sessionAttribute("flash", "Страница успешно добавлена!");
+        }
+        ctx.redirect("/urls");
+    };
+}
