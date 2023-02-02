f05b22e41011a501d6ac0b5ee7586b5108c4f6ce
Илья Остапенко <6londo9@MacBook-Pro-Ila.local>
2023-02-02 19:36:54 +1100

    clear local db

--- app/src/main/java/hexlet/code/domain/Url.java
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -3,7 +3,6 @@ package hexlet.code.domain;
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 import lombok.Getter;
-import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
